2fa1a6634a01780d7a07568847c5a4a8f2e1a468
jashmehta3300 <jashmehta3300@gmail.com>
2023-06-22 12:51:55 -0400

    linting and comments in customer-auth

--- customer-auth/controllers/userController.js
+++ customer-auth/controllers/userController.js
@@ -1,7 +1,7 @@
-import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
-import generateToken from '../utils/generateToken.js';
-import { Worker } from 'worker_threads';
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+import { Worker } from "worker_threads";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
@@ -38,12 +38,12 @@ const authUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(401);
-    throw new Error('Invalid email or password');
+    throw new Error("Invalid email or password");
   }
 });
 
 // @desc    Register a new user
-// @route   POST /auth/users
+// @route   POST /auth/users/
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -52,7 +52,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (userExists) {
     res.status(400);
-    throw new Error('User already exists');
+    throw new Error("User already exists");
   }
 
   const user = await User.create({
@@ -71,23 +71,23 @@ const registerUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(400);
-    throw new Error('Invalid user data');
+    throw new Error("Invalid user data");
   }
 });
 
-// @desc    Logout user / clear cookie
-// @route   POST /auth/users/logout
+// @desc    Logout user and clear cookie
+// @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.cookie('jwt', '', {
+  res.cookie("jwt", "", {
     httpOnly: true,
     expires: new Date(0),
   });
-  res.status(200).json({ message: 'Logged out successfully' });
+  res.status(200).json({ message: "Logged out successfully" });
 };
 
 // @desc    Get user profile
-// @route   GET /auth/users/profile
+// @route   GET api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
@@ -100,12 +100,12 @@ const getUserProfile = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error('User not found');
+    throw new Error("User not found");
   }
 });
 
 // @desc    Update user profile
-// @route   PUT /auth/users/profile
+// @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
@@ -127,9 +127,10 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error('User not found');
+    throw new Error("User not found");
   }
 });
+
 export {
   authUser,
   registerUser,
--- customer-auth/server.js
+++ customer-auth/server.js
@@ -1,46 +1,55 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+import userRoutes from "./routes/userRoutes.js";
+
+// Load environment variables from .env file
 dotenv.config();
-import connectDB from './config/db.js';
-import cookieParser from 'cookie-parser';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import userRoutes from './routes/userRoutes.js';
 
-const port = process.env.PORT || 5000;
-
+// connect to MongoDB Atlas database
+import connectDB from "./config/db.js";
 connectDB();
 
+const port = process.env.PORT || 5000;
 const app = express();
 
+// mounting middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use(cookieParser());
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use('/api/users', userRoutes);
+// mounting routes
+app.use("/api/users", userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
+// Serve static files and handle client-side routing in production mode
+if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/ui/dist')));
+  app.use(express.static(path.join(__dirname, "/ui/dist")));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'ui', 'dist', 'index.html'))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "ui", "dist", "index.html"))
   );
 } else {
-  app.get('/', (req, res) => {
-    res.send('customer-auth API is running....');
+  app.get("/", (req, res) => {
+    res.send("customer-auth API is running....");
   });
 }
 
+// error handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`customer-api server started on port ${port}`));
+app.listen(port, () =>
+  console.log(`customer-api server started on port ${port}`)
+);
