b3edcce951366913ae776f54f9898bd5d259e1d0
jashmehta3300 <jashmehta3300@gmail.com>
2023-07-03 20:05:05 -0400

    deployment on EKS v1 🚀

---
+++ docker-hub.sh
@@ -0,0 +1,31 @@
+# UI
+docker tag martian-bank-ui jashmehta3300/martian-bank-ui
+docker push jashmehta3300/martian-bank-ui
+
+# Nginx
+docker tag martian-bank-nginx jashmehta3300/martian-bank-nginx
+docker push jashmehta3300/martian-bank-nginx
+
+# customer-auth
+docker tag martian-bank-customer-auth jashmehta3300/martian-bank-customer-auth
+docker push jashmehta3300/martian-bank-customer-auth
+
+# atm-locator
+docker tag martian-bank-atm-locator jashmehta3300/martian-bank-atm-locator
+docker push jashmehta3300/martian-bank-atm-locator
+
+# dashboard
+docker tag martian-bank-dashboard jashmehta3300/martian-bank-dashboard
+docker push jashmehta3300/martian-bank-dashboard
+
+# accounts
+docker tag martian-bank-accounts jashmehta3300/martian-bank-accounts
+docker push jashmehta3300/martian-bank-accounts
+
+# transactions
+docker tag mb-transactions jashmehta3300/martian-bank-transactions
+docker push jashmehta3300/martian-bank-transactions
+
+# loan
+docker tag martian-bank-loan jashmehta3300/martian-bank-loan
+docker push jashmehta3300/martian-bank-loan
