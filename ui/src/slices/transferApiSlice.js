6c20d934411765a863678db55032bff33537d28e
jashmehta3300 <jashmehta3300@gmail.com>
2023-07-17 13:22:50 -0400

    fixed zelle integration issue ✅

--- ui/src/slices/transferApiSlice.js
+++ ui/src/slices/transferApiSlice.js
@@ -1,20 +1,19 @@
 import { apiSlice } from "./usersApiSlice";
 const transferUrl = import.meta.env.VITE_TRANSFER_URL;
 const zelleUrl = import.meta.env.VITE_ZELLE_URL;
-console.log(zelleUrl);
 
 export const transferApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     postTransfer: builder.mutation({
       query: (data) => ({ 
-        url: transferUrl, 
+        url: `${transferUrl}`, 
         method: "POST", 
         body: data 
       }),
     }),
     postTransferExternal: builder.mutation({
       query: (data) => ({
-        url: zelleUrl,
+        url: `${zelleUrl}`,
         method: "POST",
         body: data,
       }),
