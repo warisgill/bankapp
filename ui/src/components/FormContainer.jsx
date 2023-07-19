90e6dc795fb950c490d9cd1d1d2237bc2aab6b77
jashmehta3300 <jashmehta3300@gmail.com>
2023-07-19 13:44:18 -0400

    Card added in UI

--- ui/src/components/FormContainer.jsx
+++ ui/src/components/FormContainer.jsx
@@ -4,22 +4,44 @@ const FormContainer = ({ children, position }) => {
   return (
     <Container>
       {position === "left" ? (
-        <Row className="justify-content-md-left card" style={{margin: '5vh'}}>
-          <Col xs={12} md={5} className=" border p-5">
-            {children}
-          </Col>
-          {/* <Col xs={12} md={5} className="p-5" style={{  padding: "10vh" }}>
-            <img
-              src="./src/assets/card.png"
-              alt="card"
-              width="100%"
-              style={{ height: "65vh", backgroundColor: "transparent" }}
-            />
-          </Col> */}
-        </Row>
+        <>
+          <Row className="justify-content-md bg-light" style={{ margin: "5vh"}}>
+            <Col
+              md={5}
+              className="rounded border p-5"
+              style={{ margin: "5vh" }}
+            >
+              {children}
+            </Col>
+
+            <Col
+              md={6}
+              className="rounded p-5"
+              style={{ margin: "5vh" }}
+            >
+              <Row>
+                <Col md={12} className="p-5">
+                  <h1 className="text-center">$100 bonus on us!</h1>
+                  <p className="text-center">
+                    Open an eligible account with qualifying electronic deposits and get $100 bonus.
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12} className="p-5">
+                  <img
+                    src="./src/assets/card.png"
+                    alt="card"
+                    className="img-fluid"
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </>
       ) : (
         <Row className="justify-content-md-center mt-5">
-          <Col xs={12} md={6} className="card p-5">
+          <Col md={6} className="card p-5">
             {children}
           </Col>
         </Row>
