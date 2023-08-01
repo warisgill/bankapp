4c9b95b31d774f3beaf5bea61cbb46e470d52088
jashmehta3300 <jashmehta3300@gmail.com>
2023-08-01 12:19:48 -0400

    Licenses generated only for packages used

--- license_report.sh
+++ license_report.sh
@@ -15,24 +15,22 @@ brew install jq
 # Clear existing licenses.txt file (if any)
 echo -n > ./licenses.txt
 
-# report for Python microservices
-echo -e "\n\n --- Python Microservices --- \n" >> ./licenses.txt
+# report for Python microservicesxs
 python3 python_licenses.py >> ./licenses.txt
 
 # report for ui
-echo -e "\n\n --- Node.js Microservices --- \n" >> ./licenses.txt
 cd ui
 license-checker-rseidelsohn --json --production --direct  | jq -r 'to_entries | map([(.key | split("@"))[-2], (.key | split("@"))[-1], .value.licenses] | @tsv) | .[]' | column -t -s $'\t' >>../licenses.txt
 cd ..
 
 # report for customer-auth
-echo -e "\n\n --- Customer Auth --- \n" >> ./licenses.txt
 cd customer-auth
 license-checker-rseidelsohn --json --production --direct  | jq -r 'to_entries | map([(.key | split("@"))[-2], (.key | split("@"))[-1], .value.licenses] | @tsv) | .[]' | column -t -s $'\t' >>../licenses.txt
 cd ..
 
 # report for atm-locator
-echo -e "\n\n --- ATM Locator -- \n" >> ./licenses.txt
 cd atm-locator
 license-checker-rseidelsohn --json --production --direct  | jq -r 'to_entries | map([(.key | split("@"))[-2], (.key | split("@"))[-1], .value.licenses] | @tsv) | .[]' | column -t -s $'\t' >>../licenses.txt
 cd ..
+
+echo "License report generated successfully!"
