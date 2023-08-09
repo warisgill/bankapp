a18aea9a6032388f31beac0c28ec52796b64d880
jashmehta3300 <jashmehta3300@gmail.com>
2023-08-09 19:11:25 -0400

    API clarity link added to integrations folder

--- integrations/apiclarity/apiclarity.sh
+++ integrations/apiclarity/apiclarity.sh
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+## API Clarity: https://github.com/openclarity/apiclarity
+
 #######################################################################################
 ## OPTIONAL STEPS:
 
