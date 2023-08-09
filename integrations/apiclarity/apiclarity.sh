3cb2a019f8671cde1948437a92200c1eaf850257
jashmehta3300 <jashmehta3300@gmail.com>
2023-08-09 15:58:32 -0400

    locust issue resolved on EKS

--- integrations/apiclarity/apiclarity.sh
+++ integrations/apiclarity/apiclarity.sh
@@ -28,23 +28,23 @@ kubectl label namespace bank-app istio-injection=enabled
 
 # install martianbank (without NGINX)
 
-helm install --set 'nginx.enabled=false' martianbank martianbank -n bank-app
-kubectl get pods -n bank-app
+helm install --set 'nginx.enabled=false' martianbank martianbank -n bank-app & sleep 20
+kubectl get pods -n bank-app 
 kubectl get services -n bank-app
 
 # install API Clarity (note the namespace for envoyWasm)
 
-helm install --set 'trafficSource.envoyWasm.enabled=true' --set 'trafficSource.envoyWasm.namespaces={bank-app}' --set 'supportExternalTraceSource.enabled=true' --create-namespace apiclarity apiclarity/apiclarity -n apiclarity
-kubectl get pods -n apiclarity
+helm install --set 'trafficSource.envoyWasm.enabled=true' --set 'trafficSource.envoyWasm.namespaces={bank-app}' --set 'supportExternalTraceSource.enabled=true' --create-namespace apiclarity apiclarity/apiclarity -n apiclarity & sleep 20
+kubectl get pods -n apiclarity 
 kubectl get services -n apiclarity
 
 # make UI as load balancer
 kubectl apply -f ./integrations/apiclarity/lb-apiclarity.yaml -n apiclarity
 
-# after these steps, copy the dashboard external IP (kubectl get services -n bank-app) and run this commmand:
+# after these steps, run this commmand to set the dashboard IP:
 dashboard_external_ip=$(kubectl get services -n bank-app | grep dashboard | awk '{print $4}')
 echo $dashboard_external_ip
-helm upgrade martianbank martianbank -n bank-app --set 'nginx.enabled=false' --set "nginx.dashboardIP=$dashboard_external_ip" 
+helm upgrade martianbank martianbank -n bank-app --set 'nginx.enabled=false' --set "nginx.dashboardIP=$dashboard_external_ip" & sleep 40
 
 # To generate load using locust
 locust_pod=$(kubectl get pods -n bank-app | grep locust | awk '{print $1}')
